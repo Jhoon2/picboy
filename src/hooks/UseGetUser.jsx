4b6fc5a7ca770e8f05451e69fcc4222446ff3bdd
minimi61 <new1573@naver.com>
2022-10-03 14:04:51 +0900

    병합 전 정리

--- src/hooks/UseGetUser.jsx
+++ src/hooks/UseGetUser.jsx
@@ -8,7 +8,7 @@ const myToken = getCookieToken();
 
 const UseGetUser = () => {
   const [user, setUser] = useState('')
-  
+
   const readUser = async () => {
     const response = await instance.get(`/main/user-info`,
     )
